56e2a0dcc013104ae840450e8067fbeedf31f6f3
pauleziemefe <99457904+pauleziemefe@users.noreply.github.com>
2022-03-23 16:11:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+//Change rate value when it is inputter
+function updateRate() {
+  var rate_val = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rate_val;
+}
+
+//Actions for compute button
 function compute() {
-  p = document.getElementById("principal").value;
-  r = document.getElementById("rate").value;
-  t = document.getElementById("years").value;
-  a = document.getElementById("result").value;
-  document.getElementById("word").innerHTML = "You will receive an amount of ";
-  document.getElementById("result").innerHTML = p * (1 + r * t);
-  document.getElementById("message").innerHTML = "in the year ";
-  const currentYear = new Date();
-  document.getElementById("current").innerHTML =
-    parseInt(t) + currentYear.getFullYear();
+  //get values
+  var p = document.getElementById("principal").value;
+  var r = document.getElementById("rate").value;
+  var n = document.getElementById("years").value;
+
+  //calculate interest
+  var interest = (p * n * r) / 100;
+
+  //when principal is less than 0
+  if (p <= 0) {
+    window.alert("Enter a positive number");
+    document.focus("principal");
+  }
+  //when principal is greater than 0
+  else if (p > 0) {
+    var year = new Date().getFullYear() + parseInt(n);
+    document.getElementById("result").innerHTML = interest;
+    document.getElementById("word").innerHTML = "If you deposit ";
+    document.getElementById("p").innerHTML = p;
+    document.getElementById("text").innerHTML = "at an interest rate of ";
+    document.getElementById("ratee").innerHTML = r;
+    document.getElementById("per").innerHTML = "%";
+    document.getElementById("message").innerHTML =
+      "You will recieve an amount of ";
+    document.getElementById("resultt").innerHTML = interest;
+    document.getElementById("textt").innerHTML = "in the year ";
+    document.getElementById("year").innerHTML = year;
+  }
 }
